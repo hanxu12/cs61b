285afff99ce48704d47e0a2d0ca88457c906e3fe
han <xuhan@lafayette.edu>
2019-07-31 10:29:41 +0800

    lab2 finished3

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -83,8 +83,9 @@ public class IntList {
     public static IntList dcatenate(IntList A, IntList B) {
         IntList C = A;
         if (A == null){
-            return null;
+            return B;
         }
+        
         while (A.rest != null){
             A = A.rest;
         }

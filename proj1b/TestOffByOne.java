82f0fcdb8a4b138c28004002ce5c88295b762e94
han <xuhan@lafayette.edu>
2019-08-06 11:42:55 +0800

    TestOffByOne for equalChars

--- proj1b/TestOffByOne.java
+++ proj1b/TestOffByOne.java
@@ -2,11 +2,44 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class TestOffByOne {
-    /*
+
     // You must use this CharacterComparator and not instantiate
     // new ones, or the autograder might be upset.
     static CharacterComparator offByOne = new OffByOne();
 
     // Your tests go here.
-    Uncomment this class once you've created your CharacterComparator interface and OffByOne class. **/
+   // Uncomment this class once you've created your CharacterComparator interface and OffByOne class.
+    //Alphabetical chars
+    public void testEqualchar1() {
+        OffByOne obo = new OffByOne();
+        assertTrue(obo.equalChars('a', 'b'));
+        assertTrue(obo.equalChars('b', 'c'));
+    }
+
+    //Alphabetical chars
+    public void testEqualchar2() {
+        OffByOne obo = new OffByOne();
+        assertTrue(obo.equalChars('x', 'y'));
+        assertTrue(obo.equalChars('x', 'w'));
+    }
+
+    //Alphabetical chars returns false
+    public void testEqualchar3() {
+        OffByOne obo = new OffByOne();
+        assertFalse(obo.equalChars('a', 'e'));
+        assertFalse(obo.equalChars('a', 'a'));
+    }
+
+    //Non-alphabetical characters
+    public void testEqualchar4() {
+        OffByOne obo = new OffByOne();
+        assertTrue(obo.equalChars('&', '%'));
+    }
+
+    //Capital letters should return false
+    public void testEqualchar5() {
+        OffByOne obo = new OffByOne();
+        assertFalse(obo.equalChars('A', 'a'));
+        assertFalse(obo.equalChars('G', 'g'));
+    }
 }

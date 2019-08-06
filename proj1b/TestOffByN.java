dfc9a708fb84af6b4e0806babdacc17048a56d1a
han <xuhan@lafayette.edu>
2019-08-06 19:36:12 +0800

    other modifications2

--- proj1b/TestOffByN.java
+++ proj1b/TestOffByN.java
@@ -4,13 +4,13 @@ import static org.junit.Assert.*;
 public class TestOffByN {
 
     @Test
-    public void testOffByN1(){
+    public void testOffByN1() {
         OffByN offBy5 = new OffByN(5);
         assertTrue(offBy5.equalChars('a', 'f'));
     }
 
     @Test
-    public void testOffByN2(){
+    public void testOffByN2() {
         OffByN offBy4 = new OffByN(4);
         assertTrue(offBy4.equalChars('a', 'e'));
     }

f7703edb0d0008e28eaabb15fd5e7482f8fdd58c
im8a <xuhan@lafayette.edu>
2020-01-11 23:37:04 -0500

    hw2 almost done

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -3,5 +3,122 @@ package hw2;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
+    private boolean[][] grid;
+    private int openSitesCnt;
+    private WeightedQuickUnionUF checker1;
+    private WeightedQuickUnionUF checker2;
+    public Percolation(int N) {
+        grid = new boolean[N][N];
+        if (N <= 0) {
+            throw new IllegalArgumentException("Wrong input");
+        }
+        //initialize the grid
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                grid[i][j] = false;
+            }
+        }
+        openSitesCnt = 0;
+        //checker1 checks isFull, checker2 checks whether percolates
+        checker1 = new WeightedQuickUnionUF(N * N + 1);
+        checker2 = new WeightedQuickUnionUF(N * N + 2);
+        //virtual top site for both checkers
+        for (int i = 0; i < N; i++) {
+            checker1.union(N * N, i);
+        }
+        for (int i = 0; i < N; i++) {
+            checker2.union(N * N, i);
+        }
+        //virtual btm site
+        for (int i = 0; i < N; i++) {
+            checker2.union(N * N + 1, N * N - i);
+        }
+    }
+
+    public void open(int row, int col) {
+        if (!checkValid(row, col)) {
+            throw new IndexOutOfBoundsException("Out of bound");
+        }
+        if (isOpen(row, col)) {
+            return;
+        }
+        grid[row][col] = true;
+        openSitesCnt += 1;
+        //calculate row
+        int currIndex = getIndex(row, col);
+
+        //connect neighbour open sites
+        if (checkValid(row - 1, col) && isOpen(row - 1, col)) {
+            checker1.union(currIndex, getIndex(row - 1, col));
+            checker2.union(currIndex, getIndex(row - 1, col));
+        }
+        if (checkValid(row + 1, col) && isOpen(row + 1, col)) {
+            checker1.union(currIndex, getIndex(row + 1, col));
+            checker2.union(currIndex, getIndex(row + 1, col));
+        }
+        if (checkValid(row, col - 1) && isOpen(row, col - 1)) {
+            checker1.union(currIndex, getIndex(row, col - 1));
+            checker2.union(currIndex, getIndex(row, col - 1));
+        }
+        if (checkValid(row, col + 1) && isOpen(row, col + 1)) {
+            checker1.union(currIndex, getIndex(row, col + 1));
+            checker2.union(currIndex, getIndex(row, col + 1));
+        }
+    }
+
+    private int getIndex(int row, int col) {
+        return (row * grid.length + col);
+    }
+
+    private boolean checkValid(int row, int col) {
+        if (row < 0 || row >= grid.length || col < 0 || col >= grid.length) {
+            return false;
+        }
+        return true;
+    }
+
+    public boolean isOpen(int row, int col) {
+        if (!checkValid(row, col)) {
+            throw new IndexOutOfBoundsException("Out of bound");
+        }
+        return grid[row][col];
+    }
+
+    public boolean isFull(int row, int col) {
+        if (!checkValid(row, col)) {
+            throw new IndexOutOfBoundsException("Out of bound");
+        }
+        int n = grid.length;
+        return (isOpen(row, col) && checker1.connected(n * n, getIndex(row, col)));
+//        for (int i = 0; i < grid.length; i++){
+//            if (checker1.connected(i, getIndex(row, col)) == true){
+//                return true;
+//            }
+//
+//        }
+//        return false;
+    }
+
+    public int numberOfOpenSites() {
+        return openSitesCnt;
+    }
+
+    public boolean percolates() {
+        if (grid.length < 1) {
+            return false;
+        }
+        return checker2.connected(grid.length * grid.length, grid.length * grid.length + 1);
+//        for (int i = 0; i < grid.length; i++){
+//            if (isFull(grid.length-1, i)){
+//                return true;
+//            }
+//        }
+//        return false;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
 
 }
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,50 @@
 package hw2;
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
 
 public class PercolationStats {
+    double[] statArr;
+    int tempT;
+    public PercolationStats(int N, int T, PercolationFactory pf) {
+        if (N <= 0 || T <= 0) {
+            throw new IllegalArgumentException("Wrong input");
+            //throw new RuntimeException("Ring Buffer Overflow");
+        }
+        Percolation a = pf.make(N);
+        tempT = T;
+        statArr = new double[T];
+        for (int i = 0; i < T; i++) {
+            for (int j = 0; j < N * N; j++) {
 
+                while (!a.percolates()) {
+                int randRow = StdRandom.uniform(N);
+                int randCol = StdRandom.uniform(N);
+                while (!a.isOpen(randRow, randCol)){
+                    randRow = StdRandom.uniform(N);
+                    randCol = StdRandom.uniform(N);
+                }
+                a.open(StdRandom.uniform(N), StdRandom.uniform(N));
+                }
+
+                statArr[i] = a.numberOfOpenSites() / ((double) N * N);
+            }
+        }
+
+    }
+
+    public double mean() {
+        return StdStats.mean(statArr);
+    }
+
+    public double stddev() {
+        return StdStats.stddev(statArr);
+    }
+
+    public double confidenceLow() {
+        return  (mean() - 1.96 * stddev() / Math.sqrt(tempT));
+    }
+
+    public double confidenceHigh() {
+        return  (mean() + 1.96 * stddev() / Math.sqrt(tempT));
+    }
 }
